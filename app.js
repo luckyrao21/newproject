4a812cad43549c42940e9bcaf3b7c7e243966c6a
Rao-Lohitashva <95473887+Rao-Lohitashva@users.noreply.github.com>
2022-03-31 01:40:14 +0530

    this is also mine

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express=require('express');
 const mongoose=require('mongoose');
 const customerRoute=require('./routes/customer.route')
 const bodyparser=require('body-parser')
+const cors=require('cors')
 const port=process.env.PORT||3000;
 const app=express();
 
-
+app.use(cors());
 app.use(bodyparser.urlencoded({extended:true}));
 app.use(bodyparser.json());
 mongoose.connect("mongodb+srv://lucky:1234@cluster1.bvxkm.mongodb.net/testDB?retryWrites=true&w=majority").then(()=>{
