fe6ef36af8dae3ad8b8a1c5f57441c6c95284f40
Rao-Lohitashva <95473887+Rao-Lohitashva@users.noreply.github.com>
2022-03-30 23:53:01 +0530

    this is also mine

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express=require('express');
 const mongoose=require('mongoose');
 const customerRoute=require('./routes/customer.route')
 const bodyparser=require('body-parser')
+const port=process.env.PORT||3000;
 const app=express();
 
 
@@ -15,6 +16,6 @@ mongoose.connect("mongodb+srv://lucky:1234@cluster1.bvxkm.mongodb.net/testDB?ret
 
 app.use('/',customerRoute);
 
-app.listen(3000,()=>{
+app.listen(port,()=>{
     console.log("server is going on")
 })
